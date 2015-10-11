cf2c3395b603ac693c4b8970a9906edff3cddfc3
Dan Gilkerson <contact@dangilkerson.com>
2015-10-11 00:22:08 +0000

    Use new tab to hypersurf

--- client/main.js
+++ client/main.js
@@ -4,3 +4,35 @@ Template.main.helpers({
         return wave ? wave.link:null;
     }
 });
+
+var surfTab = null;
+
+Template.main.events({
+  'click .surf': function(e) {
+    e.preventDefault();
+    var link = $(".url").val();
+    
+    var wave = Waves.findOne({});
+    
+    if(!surfTab)
+      surfTab = window.open(link, link);
+    else
+      surfTab.location.href = link;
+    
+    Waves.update(wave._id, {$set:{
+      link: link
+    }});
+  }
+})
+
+
+Tracker.autorun(function(){
+  var wave = Waves.findOne({});
+
+  if(!wave) return;
+  var link = wave.link;
+  if(!surfTab)
+    surfTab = window.open(link, link);
+  else
+    surfTab.location.href = link;
+})
